a6e04d4a352f6db02395439799ca8639de6d6f67
saiedhossien99 <saiedhossien99@gmail.com>
2023-04-09 02:44:39 +0600

    class to className

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,23 +1,23 @@
 export default function Navbar() {
   return (
-    <ul class="flex">
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="#">
+    <ul className="flex">
+      <li className="mr-6">
+        <a className="text-blue-500 hover:text-blue-800" href="#">
           Active
         </a>
       </li>
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="#">
+      <li className="mr-6">
+        <a className="text-blue-500 hover:text-blue-800" href="#">
           Link
         </a>
       </li>
-      <li class="mr-6">
-        <a class="text-blue-500 hover:text-blue-800" href="#">
+      <li className="mr-6">
+        <a className="text-blue-500 hover:text-blue-800" href="#">
           Link
         </a>
       </li>
-      <li class="mr-6">
-        <a class="text-gray-400 cursor-not-allowed" href="#">
+      <li className="mr-6">
+        <a className="text-gray-400 cursor-not-allowed" href="#">
           Disabled
         </a>
       </li>
